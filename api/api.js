001f13af9699c757250843f2d283a3fb48b5b217
Van Der Elst <je.vanderelst@gmail.com>
2025-04-05 16:28:42 +0000

    added meal #persons logic

--- api/api.js
+++ api/api.js
@@ -12,6 +12,9 @@ export const food_nutrient_csv = "./fooddata/food_nutrient.csv";
 export const food_category_csv = "./fooddata/food_category.csv";
 
 export const selected_meals = new Set();
+export const mealQuantities = new Map();
+
+
 export const selected_foods = {}
 export const selected_nutrients = {}
 
