47beb268996669fcc8cca04b7ad314b01b754a21
Van Der Elst <je.vanderelst@gmail.com>
2025-04-08 11:47:05 +0000

    update links

--- api/api.js
+++ api/api.js
@@ -16,27 +16,23 @@ export const mealQuantities = new Map();
 
 export const selected_foods = {}
 export const selected_nutrients = {}
-//export const foodRegex = /\{(\d+(?:\.\d+)?)\s*[a-zA-Z]+\s*of\s*[^\{]*?\{([^}]+)\}\}/g;
-//export const foodRegex = /(?:\{([^{}]+)\s*of\s*\{([^{}]+)\}\})|(\{{2}([^{}]+)\}{2})/g;
-//export const foodRegex = /(?:\{([^{}]+)\s*of\s*[^\{]*?\{([^{}]+)\}\})|(\{{2}([^{}]+)\}{2})/g;
-//export const foodRegex = /\{(\d+(?:\.\d+)?)\s*g\s*of\s*[^\{]*?\{([^}]+)\}\}/g;
 
 const foodRegex = /(?:\{([^{}]+)\s*of\s*[^{]*\{([^{}]+)\}\})|(?:\{{2}([^{}]+)\}{2})/g;
 
+/*
+    {pinch each of {dried oregano}} → [null, "dried oregano", "{pinch each of {dried oregano}}"]
+    {{thyme}} → [null, "thyme", "{{thyme}}"]
+    {{red pepper flakes}} → [null, "red pepper flakes", "{{red pepper flakes}}"]
+    {1-2 cloves of {garlic}} → [null, "garlic", "{1-2 cloves of {garlic}}"]
+    {2 1/4 teaspoons of {instant yeast}} → [null, "instant yeast", "{2 1/4 teaspoons of {instant yeast}}"]
+    {150 g of {salt}} → [150, "salt", "{150 g of {salt}}"]
+    {150g of {salt}} → [150, "salt", "{150g of {salt}}"]
+    {120g of ripe {Bananas, ripe and slightly ripe, raw}} [120, "Bananas, ripe and slightly ripe, raw", "{120g of ripe {Bananas, ripe and slightly ripe, raw}}"]
+*/
+
 export function parseIngredients(content) {
-    /*
-        {pinch each of {dried oregano}} → [null, "dried oregano", "{pinch each of {dried oregano}}"]
-        {{thyme}} → [null, "thyme", "{{thyme}}"]
-        {{red pepper flakes}} → [null, "red pepper flakes", "{{red pepper flakes}}"]
-        {1-2 cloves of {garlic}} → [null, "garlic", "{1-2 cloves of {garlic}}"]
-        {2 1/4 teaspoons of {instant yeast}} → [null, "instant yeast", "{2 1/4 teaspoons of {instant yeast}}"]
-        {150 g of {salt}} → [150, "salt", "{150 g of {salt}}"]
-        {150g of {salt}} → [150, "salt", "{150g of {salt}}"]
-        {120g of ripe {Bananas, ripe and slightly ripe, raw}} [120, "Bananas, ripe and slightly ripe, raw", "{120g of ripe {Bananas, ripe and slightly ripe, raw}}"]
-    */
     const matches = [];
     let match;
-
     while ((match = foodRegex.exec(content)) !== null) {
         const fullMatch = match[0]; // The whole matched string
         if (match[1] && match[2]) { // From the first pattern
